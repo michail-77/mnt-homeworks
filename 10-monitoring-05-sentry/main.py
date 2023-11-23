00b0251045d83aee09b12fc650983a62194319b7
michail-77 <mail@mail>
2023-11-23 16:20:19 +0300

    1

--- 10-monitoring-05-sentry/main.py
+++ 10-monitoring-05-sentry/main.py
@@ -4,8 +4,6 @@ sentry_sdk.init(
     dsn="https://8baf98794ef8dc0878aebbe332e3542e@o4506274857353216.ingest.sentry.io/4506275295723520",
     environment="development",
     release="1.0",
-    traces_sample_rate=1.0,
-    profiles_sample_rate=1.0,
 )
 
 if __name__ == "__main__":
