db698bf3a85152fad5354c32b6a92b7e523ac692
Mikhail <113123476+michail-77@users.noreply.github.com>
2023-11-23 15:09:16 +0300

    1

--- 10-monitoring-05-sentry/main.py
+++ 10-monitoring-05-sentry/main.py
@@ -2,6 +2,7 @@ import sentry_sdk
 
 sentry_sdk.init(
     dsn="https://8ca7fe4470dc0fcf7f839af3e1f1234e@o4506274857353216.ingest.sentry.io/4506274933178368",
+    enable_tracing=True
     environment="development"
     release="1.0"
 )
